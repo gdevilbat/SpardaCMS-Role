0dff6d62d066c7cface8ac80227e19fbb4fd86a4
unknown <maldinuribrahim@gmail.com>
2019-05-15 02:08:23 +0700

    Register Menu

---
+++ Resources/views/admin/v_1/content/sidebar.blade.php
@@ -0,0 +1,12 @@
+<li class="m-menu__item  {{url()->full() == action('\Gdevilbat\SpardaCMS\Modules\Role\Http\Controllers\RoleController@index') ? 'm-menu__item--active' : ''}}" aria-haspopup="true"}>
+    <a href="{{action('\Gdevilbat\SpardaCMS\Modules\Role\Http\Controllers\RoleController@index')}}" class="m-menu__link ">
+        <i class="m-menu__link-icon flaticon-safe-shield-protection"></i>
+        <span class="m-menu__link-title"> 
+            <span class="m-menu__link-wrap"> 
+                <span class="m-menu__link-text">
+                    Role
+                </span>
+             </span>
+         </span>
+     </a>
+</li>
