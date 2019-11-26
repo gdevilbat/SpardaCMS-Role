6fb8a4e056e201d3068b408421935fe0962fe930
unknown <maldinuribrahim@gmail.com>
2019-11-26 18:47:45 +0700

    Menu Authorization

--- resources/views/admin/v_1/templates/sidebar.blade.php
+++ resources/views/admin/v_1/templates/sidebar.blade.php
@@ -1,12 +1,14 @@
-<li class="m-menu__item  {{Route::current()->getName() == 'role' ? 'm-menu__item--active' : ''}}" aria-haspopup="true">
-    <a href="{{action('\Gdevilbat\SpardaCMS\Modules\Role\Http\Controllers\RoleController@index')}}" class="m-menu__link ">
-        <i class="m-menu__link-icon flaticon-safe-shield-protection"></i>
-        <span class="m-menu__link-title"> 
-            <span class="m-menu__link-wrap"> 
-                <span class="m-menu__link-text">
-                    Role
-                </span>
+@can('menu-role')
+    <li class="m-menu__item  {{Route::current()->getName() == 'role' ? 'm-menu__item--active' : ''}}" aria-haspopup="true">
+        <a href="{{action('\Gdevilbat\SpardaCMS\Modules\Role\Http\Controllers\RoleController@index')}}" class="m-menu__link ">
+            <i class="m-menu__link-icon flaticon-safe-shield-protection"></i>
+            <span class="m-menu__link-title"> 
+                <span class="m-menu__link-wrap"> 
+                    <span class="m-menu__link-text">
+                        Role
+                    </span>
+                 </span>
              </span>
-         </span>
-     </a>
-</li>
+         </a>
+    </li>
+@endcan
